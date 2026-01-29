89ed1d7181fd011d6f59da1074f7120f9216dbe9
hannatammela <hanna.tammela@seamk.fi>
2026-01-29 19:05:27 +0200

    notifikaatiot toimii osittain

--- app/NewList.tsx
+++ app/NewList.tsx
@@ -1,14 +1,26 @@
 
 import { DancingScript_400Regular, useFonts } from '@expo-google-fonts/dancing-script';
 import { MaterialIcons } from '@expo/vector-icons';
+import * as Notifications from 'expo-notifications';
 import { router } from 'expo-router';
 import { useEffect, useState } from 'react';
-import { Pressable, Text, TextInput, View } from 'react-native';
+import { Alert, Platform, Pressable, Text, TextInput, View } from 'react-native';
 import commonStyles from "../styles/commonStyles";
 import { supabase } from '../supabaseClient';
 
+// Notifikaatioiden handler
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldPlaySound: true,
+    shouldSetBadge: false,
+    shouldShowBanner: true,
+    shouldShowList: true,
+  }),
+});
+
 export default function NewList() {
-  const [fontsLoaded] = useFonts({ DancingScript: DancingScript_400Regular });
+  const [fontsLoaded] = useFonts({ 
+    DancingScript: DancingScript_400Regular });
   
   // Lomakkeen tilat, tuote ja määrä
   const [product, setProduct] = useState('');
@@ -20,7 +32,39 @@ export default function NewList() {
     quantity: string; 
     checked: boolean 
   }[]>([]);
-  
+
+
+
+  // Notifikaatioluvat
+    useEffect(() => {
+    (async () => {
+      if (Platform.OS === 'android') {
+        await Notifications.setNotificationChannelAsync('default', {
+          name: 'Default',
+          importance: Notifications.AndroidImportance.MAX,
+        });
+      }
+
+      const { status } = await Notifications.getPermissionsAsync();
+
+      if (status !== 'granted') {
+        const { status: requestStatus } =
+          await Notifications.requestPermissionsAsync();
+
+        if (requestStatus !== 'granted') {
+          Alert.alert(
+            'Lupa tarvitaan',
+            'Notifikaatiot eivät toimi ilman lupaa'
+          );
+        }
+      }
+    })();
+  }, []);
+
+
+
+
+  // REAALIAIKAINEN KUUNTELU Realtimen avulla
   // Haetaan tuotteet ja kuunnellaan reaaliaikaisia muutoksia
    useEffect(() => {
     fetchItems();
@@ -36,6 +80,10 @@ export default function NewList() {
     (payload) => {
       console.log('Realtime event:', payload);
       fetchItems(); // Lista päivitetään muutoksien jälkeen
+
+    if ((payload.new as { product?: string })?.product) {
+      showNotification((payload.new as { product: string }).product);
+      }
     }
   )
   .subscribe(); // Websocket yhteys
@@ -47,6 +95,29 @@ export default function NewList() {
     supabase.removeChannel(channel);
   }
   }, []);
+
+
+  // Funktio notifikaatiolle
+    const showNotification = async (productName: string) => {
+    await Notifications.scheduleNotificationAsync({
+      content: {
+        title: 'Uusi tuote lisätty!',
+        body: `${productName} lisättiin listaan`,
+        ...(Platform.OS === 'android'
+          ? { channelId: 'default' }
+          : {}),
+      },
+      trigger: null, // heti näkyviin
+    });
+  };
+
+
+
+
+
+
+
+
 
   // LISÄÄMINEN
   const addProduct = async () => {
